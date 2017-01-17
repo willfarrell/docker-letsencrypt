9c5e95c68e0326d3ae31f2f0edac500ee6fc0d78
will Farrell <will.farrell@gmail.com>
2017-01-17 14:28:09 -0700

    supress echo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 #set -x
-echo "run"
+
 if [ "${1}" = 'dehydrated' ]; then
     if [ "${LE_ENV}" == 'production' ]; then
         sed -i 's@CA=.*@CA="https://acme-v01.api.letsencrypt.org/directory"@g' /etc/dehydrated/config
