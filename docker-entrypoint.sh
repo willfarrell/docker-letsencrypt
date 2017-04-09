6cca2b1e18db12067f6a5e18002703d5cf4eace7
will Farrell <will.farrell@gmail.com>
2017-04-08 18:52:37 -0600

    bug fix: $1 could be `sh` preventing prod from ever being used

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,8 @@
 set -e
 #set -x
 
-if [ "${1}" = 'dehydrated' ]; then
-    if [ "${LE_ENV}" == 'production' ]; then
-        sed -i 's@CA=.*@CA="https://acme-v01.api.letsencrypt.org/directory"@g' /etc/dehydrated/config
-    fi
+if [ "${LE_ENV}" == 'production' ]; then
+	sed -i 's@CA=.*@CA="https://acme-v01.api.letsencrypt.org/directory"@g' /etc/dehydrated/config
 fi
 
 echo "${@}"
